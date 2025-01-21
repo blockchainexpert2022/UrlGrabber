a5af0590cd7b1f2601990bf419b1b8f448837865
WDAGUtilityAccount <blockchainexpert2022@protonmail.com>
2025-01-21 14:53:36 +0100

    tests

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,7 +14,7 @@ class Program
     static async Task Main(string[] args)
     {
         // URL de la page à analyser
-        string initialPageUrl = "https://www.lemonde.fr";
+        string initialPageUrl = "https://www.lefigaro.fr";
         Uri uri = new Uri(initialPageUrl);
         baseUrl = uri.GetLeftPart(UriPartial.Authority); // Base URL (ex : https://www.microsoft.com)
         Console.WriteLine("baseUrl : " + baseUrl);
